4739dc69556c09f1191cfa5a7967f49836f4776c
danielaolivares <dany.olivaresl@gmail.com>
2020-08-21 23:02:31 -0400

    separate cloud functions and react frontend

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -9,3 +10,14 @@ admin.initializeApp();
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("hola");
 });
+exports.getScreams = functions.https.onRequest((req,res) => {
+    admin.firestore().collection('screams').get()
+    .then(data => {
+        let screams = [];
+        data.forEach(doc => {
+            this.getScreams.push(doc.data());
+            return res.jason(screams)
+        })
+        .catch(err => console.error(err))
+    })
+})
